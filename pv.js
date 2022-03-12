ecdbdf7686d809ab004afb55659d0151fbdba22d
yanuabialwi <zhalxdev@gmail.com>
2022-03-11 23:20:08 -0500

    Create pv.js

---
+++ pv.js
@@ -0,0 +1,2 @@
+jQuery.getScript("//cdn.firebase.com/js/client/2.3.2/firebase.js").done(function(){
+$.each($(".post-view[data-id]"),function(e,a){var i=$(a).parent().find("#postviews").addClass("view-load"),t=new Firebase("https://ctd-page-views-default-rtdb.firebaseio.com/pages/id/"+$(a).attr("data-id"));t.once("value",function(e){var o=e.val(),d=!1;null==o&&((o={}).value=0,o.url=window.location.href,o.id=$(a).attr("data-id"),d=!0),i.removeClass("view-load").text(o.value),o.value++,"/"!=window.location.pathname&&(d?t.set(o):t.child("value").set(o.value))})})});
